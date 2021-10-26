1483d0dce0452ca62f02c4679c8d6061215436fb
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2021-10-26 22:18:14 +0100

    update: implemented header

--- src/components/user/Index.js
+++ src/components/user/Index.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from 'assets/devchallenges.svg';
 import user from 'assets/user-avatar.png';
+import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
+import { FaUserCircle } from 'react-icons/fa';
+import { GrGroup } from 'react-icons/gr';
+import { MdLogout } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 function UserIndex() {
+    const [show, setShow] = useState(false)
+    const showHandler = () => {
+        setShow(!show)
+    }
     return (
-        <section>
-            <header>
+        <section className="user__wrapper">
+            <header className="header__user">
                 <div className='header__logo'>
-                    <img src={logo} alt="devchallenges-logo"/>
+                    <img src={logo} alt="devchallenges-logo" />
                 </div>
-                <nav>
-                    <div>
-                        <div>
-                            <img src={user} alt="user-profile"/>
+                <div>
+                    <div className="header__user--details">
+                        <div className="user__image">
+                            <img className="img" src={user} alt="user-profile" />
                         </div>
-                        <p>Xanthe Neal</p>
+                        <p className="username">Xanthe Neal</p>
+                        <div className="drop-down" onClick={showHandler}>
+                            {
+                                show ? <IoMdArrowDropdown /> :
+                                    <React.Fragment>
+                                        <IoMdArrowDropup />
+                                        <nav>
+                                            <ul>
+                                                <Link to="" className="link"><li><span className="nav__icon"><FaUserCircle /></span>My Profile</li></Link>
+                                                <Link to="" className="link"><li><span className="nav__icon"><GrGroup /></span>Group Chat</li></Link>
+                                                <Link to="" className="link"><li><span className="nav__icon"><MdLogout /></span>My LogOut</li></Link>
+                                            </ul>
+                                        </nav>
+                                    </React.Fragment>
+                            }
+                        </div>
                     </div>
-                </nav>
+                </div>
             </header>
+
+
         </section>
     )
 }
