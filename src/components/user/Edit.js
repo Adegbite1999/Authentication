33ebb3a1184ef8caf4d6e6b46446cee1cfe1b74e
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2021-11-01 08:19:09 +0100

    üpdate: added some changes

--- src/components/user/Edit.js
+++ src/components/user/Edit.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import avatar from 'assets/user-avatar.png';
+// import avatar from 'assets/user-avatar.png';
 import FormInput from 'Input/Input';
 import TextArea from 'Input/TextArea';
-import 'styles/edit.css'
+import 'styles/edit.css';
+import {FiCamera} from 'react-icons/fi';
+import {IoIosArrowBack} from 'react-icons/io';
 function Edit() {
     return (
         <React.Fragment>
             <div className="profile__edit-wrapper">
-                <p className="arrow__back"><Link to="/user/profile" style={{ textDecoration: 'none' }}>Back</Link></p>
+                <p className="arrow__back"><Link to="/user/profile" style={{ textDecoration: 'none' }}><span className="arrow"><IoIosArrowBack/>Back</span></Link></p>
                 <section className="profile__container--edit" >
                     <div className="edit__profile--intro">
                         <h5>Change Info</h5>
                         <p>Changes will be reflected to every service.</p>
                     </div>
                     <div className="user__box">
-                        <div className='user__profile--image'>
-                            <img className="img" src={avatar} alt="user placeholder" />
+                        <div className='user__profile--bg'>
+                            <input className="image__file" type="file"/>
+                            <span className="image__file--icon"><FiCamera size={25}/></span>
                         </div>
                         <h5 className="user--action">Change photo</h5>
                     </div>
